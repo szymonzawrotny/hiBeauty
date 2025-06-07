f8b9d15bc92ecf82fe8c051109cbc12ce64ce7a0
szymonzawrotny <szymonzawrotny@gmail.com>
2025-06-07 21:35:02 +0200

    added animation on burger click

--- client/components/header/Burger.tsx
+++ client/components/header/Burger.tsx
@@ -1,8 +1,18 @@
-import "@/components/header/Burger.style.scss"
+'use client';
+import { useRef } from 'react';
+import '@/components/header/Burger.style.scss';
 
 const Burger = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+
   return (
-    <div className="burger">
+    <div
+      className="burger"
+      onClick={() => {
+        if (ref.current) ref.current.classList.toggle('active');
+      }}
+      ref={ref}
+    >
       <span className="up"></span>
       <span className="down"></span>
     </div>
